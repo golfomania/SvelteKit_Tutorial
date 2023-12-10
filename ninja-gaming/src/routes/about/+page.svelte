40a058868cf9d90957f4fed4975ac9e93c59d5ec
Martin Loeffler <ml.loeffler@gmx.de>
2023-12-10 18:43:45 +0100

    first route

---
+++ ninja-gaming/src/routes/about/+page.svelte
@@ -0,0 +1,17 @@
+<div class="about">
+	<h4>About</h4>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquid tenetur aut cumque earum ab
+		ad modi deleniti, eius cum, similique repellat, harum velit porro corporis illum. Esse veniam,
+		incidunt officia doloremque numquam, modi assumenda, excepturi cum placeat sequi nisi iure
+		eligendi cupiditate ex expedita quia quaerat vel aspernatur! Illo molestias numquam sunt
+		officiis, harum quidem ex porro facilis earum? Quisquam, quod. Quisquam, quod.
+	</p>
+</div>
+
+<style>
+	.about {
+		margin: 0 auto;
+		width: 80%;
+	}
+</style>
