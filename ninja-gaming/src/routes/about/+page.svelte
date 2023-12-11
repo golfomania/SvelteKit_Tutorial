2bd380aa0f0610d088b3396744bf1dfaabedac6e
Martin Loeffler <ml.loeffler@gmx.de>
2023-12-11 21:37:36 +0100

    test

--- ninja-gaming/src/routes/about/+page.svelte
+++ ninja-gaming/src/routes/about/+page.svelte
@@ -7,6 +7,13 @@
 		eligendi cupiditate ex expedita quia quaerat vel aspernatur! Illo molestias numquam sunt
 		officiis, harum quidem ex porro facilis earum? Quisquam, quod. Quisquam, quod.
 	</p>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut aliquid tenetur aut cumque earum ab
+		ad modi deleniti, eius cum, similique repellat, harum velit porro corporis illum. Esse veniam,
+		incidunt officia doloremque numquam, modi assumenda, excepturi cum placeat sequi nisi iure
+		eligendi cupiditate ex expedita quia quaerat vel aspernatur! Illo molestias numquam sunt
+		officiis, harum quidem ex porro facilis earum? Quisquam, quod. Quisquam, quod.
+	</p>
 </div>
 
 <style>
